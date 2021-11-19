3e0fef63454a463239918a37efdacfba8463db77
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-11-18 20:11:48 -0600

    Crear carpetas y integracion de librerias de diseño primer nav y primer diseño home
    Crear carpetas y integracion de librerias de diseño primer nav y primer diseño home

---
+++ views/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Carnicos Norte | Inicio</title>
+<?php
+include('Nav.php')
+?>
+</head>
+<body>
+	<div class="row">
+		 <img class="materialboxed" width="100%" src="img/banner.jpg">
+	</div><br><br><br><br><br><br><br>
+	<div class="container row  blue-grey lighten-4">
+		<div class="col s2"><br></div>
+		<div class="col s10"><br><br><br>
+			<h1 class="hide-on-small-only">Bienvenid@ A:</h1><br>
+			<h4 class="hide-on-med-and-up">Bienvenid@ A:</h4><br>
+			<h2 class="hide-on-small-only"> <font color="#cfd8dc">-></font> <b class="red-text">Carnicos</b> <b class="amber-text">del Norte</b></h2><div class="hide-on-small-only"><br><br><br><br></div>
+			<h4 class="hide-on-med-and-up"><b class="-text">Carnicos del</b> <b class="amber-text">Norte</b></h4><div class="hide-on-med-and-up"><br><br><br><br><br><br></div>
+		</div>
+	</div>	<br><br><br>
+</div>
+</body>
+</html>
